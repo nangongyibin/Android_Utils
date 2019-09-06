862b8e1b6680e781f77de3f6d292b146a079e41b
nangongyibin7219 <nangongyibin@gmail.com>
2019-09-06 21:31:35 +0800

    Android库--流转字符串

---
+++ androidutils/src/main/java/com/ngyb/androidutils/StreamUtils.java
@@ -0,0 +1,33 @@
+package com.ngyb.androidutils;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * 作者：南宫燚滨
+ * 描述：
+ * 邮箱：nangongyibin@gmail.com
+ * 日期：2019/9/4 21:48
+ */
+public class StreamUtils {
+
+    /**
+     * @param is
+     * @return
+     */
+    public static String StreamToString(InputStream is) {
+        try {
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            int len = 0;
+            byte[] buf = new byte[1024];
+            while ((len = is.read(buf)) != -1) {
+                baos.write(buf, 0, len);
+            }
+            return baos.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "";
+        }
+    }
+}
