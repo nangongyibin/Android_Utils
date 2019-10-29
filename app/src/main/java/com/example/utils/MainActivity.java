78dcec0c79df74c39e95beda37f26f225bb91d82
nangongyibin7219 <nangongyibin@gmail.com>
2019-10-29 22:04:14 +0800

    更新

--- app/src/main/java/com/example/utils/MainActivity.java
+++ app/src/main/java/com/example/utils/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.utils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.ngyb.utils.R;
 
 public class MainActivity extends AppCompatActivity {
 
