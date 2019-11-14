c5e64c7980050278f80da13c6fcdc1ae6f0a8e1a
nangongyibin7219 <nangongyibin@gmail.com>
2019-11-14 17:33:43 +0800

    添加文件工具类和Uri工具类

---
+++ Library/src/main/java/com/ngyb/utils/FileUtils.java
@@ -0,0 +1,33 @@
+package com.ngyb.utils;
+
+import android.util.Log;
+
+/**
+ * 作者：南宫燚滨
+ * 描述：
+ * 邮箱：nangongyibin@gmail.com
+ * 日期：2019/11/14 17:26
+ */
+public class FileUtils {
+    private static final String TAG = "FileUtils";
+
+    public String getFileName(String path) {
+        if (path.isEmpty()) {
+            return "";
+        } else {
+            boolean isContains = path.contains("/");
+            if (isContains) {
+                int i = path.lastIndexOf("/");
+                return path.substring(i + 1);
+            } else {
+                return path;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        FileUtils fileUtils = new FileUtils();
+        String fileName = fileUtils.getFileName("/storage/emulated/0/360/sdk/persistence/3416a75f4cea9109507cacd8e2f2aefc");
+        System.out.println(fileName);
+    }
+}
