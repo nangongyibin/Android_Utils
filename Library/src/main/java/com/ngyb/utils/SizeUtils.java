cc95975a167c1f8c0e26b73f392c51e4c838a8a1
南宫燚滨 <nangongyibin@gmail.com>
2020-07-29 16:33:23 +0800

    添加xml工具类

---
+++ Library/src/main/java/com/ngyb/utils/SizeUtils.java
@@ -0,0 +1,122 @@
+package com.ngyb.utils;
+
+import android.content.Context;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * 作者：南宫燚滨
+ * 描述：
+ * 邮箱：nangongyibin@gmail.com
+ * 日期：2020/7/29 15:56
+ */
+public class SizeUtils {
+    /**
+     * dp转px
+     *
+     * @param context
+     * @param dpValue
+     * @return
+     */
+    public static int dp2px(Context context, float dpValue) {
+        float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
+    }
+
+    /**
+     * px转dp
+     *
+     * @param context
+     * @param pxValue
+     * @return
+     */
+    public static int px2dp(Context context, float pxValue) {
+        float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (pxValue / scale + 0.5f);
+    }
+
+    /**
+     * sp转px
+     *
+     * @param context
+     * @param spValue
+     * @return
+     */
+    public static int sp2px(Context context, float spValue) {
+        float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
+        return (int) (spValue * fontScale + 0.5f);
+    }
+
+    /**
+     * px转sp
+     *
+     * @param context
+     * @param pxValue
+     * @return
+     */
+    public static int px2sp(Context context, float pxValue) {
+        float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
+        return (int) (pxValue / fontScale + 0.5f);
+    }
+
+    /**
+     * 各种单位转换
+     *
+     * @param unit
+     * @param value
+     * @param metrics
+     * @return
+     */
+    public static float applyDimension(int unit, float value, DisplayMetrics metrics) {
+        switch (unit) {
+            case TypedValue.COMPLEX_UNIT_PX:
+                return value;
+            case TypedValue.COMPLEX_UNIT_DIP:
+                return value * metrics.density;
+            case TypedValue.COMPLEX_UNIT_SP:
+                return value * metrics.scaledDensity;
+            case TypedValue.COMPLEX_UNIT_PT:
+                return value * metrics.xdpi * (1.0f / 72);
+            case TypedValue.COMPLEX_UNIT_IN:
+                return value * metrics.xdpi;
+            case TypedValue.COMPLEX_UNIT_MM:
+                return value * metrics.xdpi * (1.0f / 25.4f);
+        }
+        return 0;
+    }
+
+    /**
+     * 在onCreate即可获取view的宽高
+     *
+     * @param view
+     * @return
+     */
+    public static int[] getViewMeasure(View view) {
+        int widthMeasureSpec = View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED);
+        int heightMeasureSpec = View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED);
+        view.measure(widthMeasureSpec, heightMeasureSpec);
+        return new int[]{view.getMeasuredWidth(), view.getMeasuredHeight()};
+    }
+
+    /**
+     * ListView中提前测量view尺寸，如HeaderView
+     * @param view
+     */
+    public static void measureView(View view){
+        ViewGroup.LayoutParams p = view.getLayoutParams();
+        if (p ==null){
+            p = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        }
+        int width = ViewGroup.getChildMeasureSpec(0, 0, p.width);
+        int height;
+        int tempHeight = p.height;
+        if (tempHeight >0){
+            height = View.MeasureSpec.makeMeasureSpec(tempHeight,View.MeasureSpec.EXACTLY);
+        }else{
+            height = View.MeasureSpec.makeMeasureSpec(0,View.MeasureSpec.UNSPECIFIED);
+        }
+        view.measure(width,height);
+    }
+}
--- Library/src/main/java/com/ngyb/utils/StringUtils.java
+++ Library/src/main/java/com/ngyb/utils/StringUtils.java
@@ -25,4 +25,56 @@ public class StringUtils extends RegularUtils {
     public static boolean isUserName(String str) {
         return isMatch(Constant.REGEX_USERNAME, str);
     }
+
+    /**
+     * 判断字符串是否不为空
+     *
+     * @param str
+     * @return
+     */
+    public static boolean isNotNull(String str) {
+        if (str != null && !"".equals(str.trim())) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * 判断输入的字符串是否为空
+     *
+     * @param str
+     * @return
+     */
+    public static boolean isNull(String str) {
+        return !isNotNull(str);
+    }
+
+    /**
+     * @param str    空字符
+     * @param custom 为空要转化的字符串
+     * @return
+     */
+    public static String NullToCustom(String str, String custom) {
+        if (isNull(custom)) {
+            if (isNull(str)) {
+                return "";
+            }
+        } else {
+            if (isNull(str)) {
+                return custom;
+            }
+        }
+        return str;
+    }
+
+    /**
+     * 如果是空转化为""
+     *
+     * @param str
+     * @return
+     */
+    public static String NullTo(String str) {
+        return NullToCustom(str, "");
+    }
 }
